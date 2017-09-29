0655e2fadd760fcfb57681f5c725d57f5accf32e
Amit <amit@Amits-MacBook-Pro.local>
2017-09-29 06:09:31 +0530

    practice basics of binary tree

--- src/main/java/com/dsalgo/BinaryTree.java
+++ src/main/java/com/dsalgo/BinaryTree.java
@@ -61,4 +61,22 @@ public class BinaryTree {
         postorderTraversal(root.right);
         System.out.print(root.val + ",");
     }
+
+    // Search in Binary Search Tree
+    // Insertion in BST(iterative method)
+    // same binary tree
+    // size of binary tree
+    // Height of binary tree
+    // Root to leaf sum binary tree
+    // Check if binary tree is BST
+    // Level order of binary tree
+    // Iterative postorder traversal of binary tree
+    // Iterative preorder traversal of binary tree
+    // Iterative inorder traversal of binary tree
+    // level by level printing of binary tree
+    // Reverse level order traversal of binary tree
+    // Tree traversal spiral order
+    // Lowest common ancestor of BST
+    // Lowest common ancestor of binary tree
+    // Largest BST in binary tree
 }
